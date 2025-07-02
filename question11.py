c9b90f18e5d7c7bba85fbdacb1e2e9d755c07cc0
Temi <limiajayi@gmail.com>
2025-07-02 14:30:23 +0100

    EZ roman numerals.

--- question11.py
+++ question11.py
@@ -7,32 +7,52 @@ class Solution:
         ptrRight = len(height) - 1
         areas = []
         
-        maxLeft = 0
-        maxRight = 0
-        length = 0
-        breadth = 0
+        width = ptrRight - ptrLeft
+        length = height[ptrLeft] if height[ptrLeft] <= height[ptrRight] else height[ptrRight]
+        area = width * length
         
-        while ptrLeft < len(height) and ptrRight >= 0:
+        areas.append(area)
+        
+        while ptrLeft < len(height) and ptrRight > 0:
+            
             if ptrLeft == ptrRight:
                 break
             
-            length = maxLeft if maxRight > maxLeft else maxRight
-            breadth = ptrRight - ptrLeft
-            areas.append(length * breadth)
-            
-            if height[ptrLeft] <= height[ptrRight]:
+            if height[ptrLeft] < height[ptrLeft + 1]:
                 ptrLeft += 1
                 
-            if height[ptrLeft] > height[ptrRight]:
+            width = ptrRight - ptrLeft
+            length = height[ptrLeft] if height[ptrLeft] <= height[ptrRight] else height[ptrRight]
+            area = width * length
+            areas.append(area)
+            
+            if height[ptrRight - 1] > height[ptrRight]:
                 ptrRight -= 1
             
-            maxLeft = height[ptrLeft]
-            maxRight = height[ptrRight]
-            
+            width = ptrRight - ptrLeft
+            length = height[ptrLeft] if height[ptrLeft] <= height[ptrRight] else height[ptrRight]
+            area = width * length
+            areas.append(area)
+                
+            if len(areas) > 5:
+                if areas[len(areas) - 2] == areas[len(areas) - 1]:
+                    break
             
         return areas
-
-
-
-
-print(Solution.maxArea([1,2,1]))
+    
+    def intToRoman(num):
+        #Ctrl + [] to change indent
+        
+        dict = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90,  "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1}
+        romans = ""
+        
+        
+        for letter in dict:
+            while num >= dict[letter]:
+                
+                romans += letter
+                num -= dict[letter]
+                
+        return romans
+        
+        
